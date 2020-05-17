29ce6c03444c9b49649153219483299ce2135491
OlsenJoseph <59711722+OlsenJoseph@users.noreply.github.com>
2020-05-17 03:05:07 -0400

    v5

--- moto.js
+++ moto.js
@@ -168,32 +168,33 @@
         
         var grid = create2DArray( NUM_CELLS_HORIZONTAL, NUM_CELLS_VERTICAL );
         var CELL_EMPTY = 0;
+        
+        var CELL_OCCUPIED = 1;
         var CELL_OCCUPIED1 = 1;
         var CELL_OCCUPIED2 = 2;
 
         
         // Current position and direction of light cycle 1
         var lightCycle1_x = NUM_CELLS_HORIZONTAL / 2;
-        var lightCycle1_y = NUM_CELLS_VERTICAL - 2;
+        var lightCycle1_y = NUM_CELLS_VERTICAL - 1;
         var lightCycle1_vx = 0; // positive for right
         var lightCycle1_vy = -1; // positive for down
         var lightCycle1_alive = true;
-        var lightCycle1_sc = 0;
         
-        grid[lightCycle1_x][lightCycle1_y] = CELL_OCCUPIED1; // to mark the initial grid cell as occupied
         
         // Current position and direction of light cycle 2
         var lightCycle2_x = NUM_CELLS_HORIZONTAL / 2;
-        var lightCycle2_y = 2 ;
+        var lightCycle2_y = -1 ;
         var lightCycle2_vx = 0; // positive for right
         var lightCycle2_vy = 1; // positive for down
         var lightCycle2_alive = true;
-        var lightCycle2_sc = 0;
 
+        grid[lightCycle1_x][lightCycle1_y] = CELL_OCCUPIED1; // to mark the initial grid cell as occupied
+        grid[lightCycle2_x][lightCycle2_y] = CELL_OCCUPIED2; // to mark the initial grid cell as occupied
 
         var endGame = false;
         
-        grid[lightCycle2_x][lightCycle2_y] = CELL_OCCUPIED2; // to mark the initial grid cell as occupied
+        
         
         function keyDownHandler(e) {
             // console.log("keyCode: " + e.keyCode );
@@ -218,11 +219,11 @@
             //light cycle 2
             else if (e.keyCode === 87) { // w key
                 lightCycle2_vx = 0;
-                lightCycle2_vy = -1;
+                lightCycle2_vy = 1;
             }
             else if (e.keyCode === 83) { // S  key
                 lightCycle2_vx = 0;
-                lightCycle2_vy = 1;
+                lightCycle2_vy = -1;
             }
             else if (e.keyCode === 68) { // D key
                 lightCycle2_vy = 0;
@@ -302,28 +303,8 @@
     
 
 
-        var redraw = function() {
-            //mouseControl();
-      
-            C.fillStyle = "#000000";
-            C.fillRect(0,0,canvas.width,canvas.height);
-        
-            for ( var i = 0; i < NUM_CELLS_HORIZONTAL; ++i ) {
-                for ( var j = 0; j < NUM_CELLS_VERTICAL; ++j ) {
-                    if ( grid[i][j] === CELL_OCCUPIED1 ){
-                        C1.fillStyle = tail1Color ;
-                        C1.fillRect( x0+i*cellSize+1, y0+j*cellSize+1, cellSize-2, cellSize-2 );
-                    }
-                    else if ( grid[i][j] === CELL_OCCUPIED2 ){
-                            C2.fillStyle = tail2Color;
-                            C2.fillRect( x0+i*cellSize+1, y0+j*cellSize+1, cellSize-2, cellSize-2 );
-                    }
-            }
-            C1.fillStyle = lightCycle1_alive ? "#ff0000" : "#FFFFFF";
-            C1.fillRect( x0+lightCycle1_x*cellSize, y0+lightCycle1_y*cellSize, cellSize, cellSize );
-            C2.fillStyle = lightCycle2_alive ? "#ff0000" : "#FFFFFF";
-            C2.fillRect( x0+lightCycle2_x*cellSize, y0+lightCycle2_y*cellSize, cellSize, cellSize );
-        
+        function gamefinish() {
+
             if(endGame){
                 endGame = false;
                 gameNumber++;
@@ -335,11 +316,57 @@
                 else {
                     player2Points++;
                 }
-                //ici dans le onclick recommencer
-                //addToDom("gameMenu", '<button id="resetButton" onclick="restartGame()" >Restart</button>');
-
-                
             }
+        }
+
+        var redraw = function() {
+            //mouseControl();
+
+
+            
+      
+            C.fillStyle = "#000000";
+            C.fillRect(0,0,canvas.width,canvas.height);
+        
+            for ( var i = 0; i < NUM_CELLS_HORIZONTAL; ++i ) {
+                for ( var j = 0; j < NUM_CELLS_VERTICAL; ++j ) {
+
+
+                    if(grid[i][j] !== CELL_EMPTY) {
+
+                        if ( grid[i][j] === CELL_OCCUPIED1 ){
+                            console.log("le grid "+grid[i][j]);
+                            C1.fillStyle = tail1Color ;
+                            C1.fillRect( x0+i*cellSize+1, y0+j*cellSize+1, cellSize-2, cellSize-2 );
+                        }
+                        else if ( grid[i][j] === CELL_OCCUPIED2 ){
+                                console.log("le grid "+grid[i][j]);
+                                C2.fillStyle = tail2Color;
+                                C2.fillRect( x0+i*cellSize+1, y0+j*cellSize+1, cellSize-2, cellSize-2 );
+                        }
+
+                    }
+
+
+
+                    if ( grid[i][j] === CELL_OCCUPIED1 ){
+                        console.log("le grid "+grid[i][j]);
+                        C1.fillStyle = tail1Color ;
+                        C1.fillRect( x0+i*cellSize+1, y0+j*cellSize+1, cellSize-2, cellSize-2 );
+                    }
+                    else if ( grid[i][j] === CELL_OCCUPIED2 ){
+                            console.log("le grid "+grid[i][j]);
+                            C2.fillStyle = tail2Color;
+                            C2.fillRect( x0+i*cellSize+1, y0+j*cellSize+1, cellSize-2, cellSize-2 );
+                    }
+                    
+            }
+            C1.fillStyle = lightCycle1_alive ? "#ff0000" : "#FFFFFF";
+            C1.fillRect( x0+lightCycle1_x*cellSize, y0+lightCycle1_y*cellSize, cellSize, cellSize );
+            C2.fillStyle = lightCycle2_alive ? "#ff0000" : "#FFFFFF";
+            C2.fillRect( x0+lightCycle2_x*cellSize, y0+lightCycle2_y*cellSize, cellSize, cellSize );
+        
+            
         
          } 
         }  
@@ -349,13 +376,7 @@
 
         //Pour continuer le jeu 
         document.getElementById('continueButton').onclick = function continueButtonHandler() {
-            /*
-            if(gamePaused) {
-                gamePaused = false;
-                /**
-                 * to-do
-                 */
-           // }
+           
             refreshGame(100);
             console.log("DANS le buton continue")
 
@@ -373,19 +394,11 @@
 
             restartGame();
             console.log("Dans le boutton restart");
-           /*  if(endGame){
-                clearInterval(refreshIntervalId);
-                endGame = false;
-                //ici dans le onclick recommencer
-                //addToDom("gameMenu", '<button id="resetButton" onclick="restartGame()" >Restart</button>');
-                restartGame();
-                
-            }*/
+    
         }
       
 
-        
-        
+
         var advance = function() {
         
             if ( lightCycle1_alive && lightCycle2_alive) {
@@ -397,26 +410,31 @@
                 var new2_y = lightCycle2_y + lightCycle2_vy;
         
                 // Check for collision with grid boundaries and with trail
-                if (
-                    new1_x < 0 || new1_x >= NUM_CELLS_HORIZONTAL || new1_y < 0 || new1_y >= NUM_CELLS_VERTICAL
-                    || grid[new1_x][new1_y] === CELL_OCCUPIED1
-                ) {
+                if ( new1_x < 0 || new1_x >= NUM_CELLS_HORIZONTAL 
+                    || new1_y < 0 || new1_y >= NUM_CELLS_VERTICAL
+                    || grid[new1_x][new1_y] || grid[new2_x][new2_y] === CELL_OCCUPIED1 && CELL_OCCUPIED2 ) {
                     lightCycle1_alive = false;
-                    console.log(lightCycle1_alive);
+                    endGame = true;
+                    console.log("joueur 1 à perdu");
                 }
                 
                 else if (
                     new2_x < 0 || new2_x >= NUM_CELLS_HORIZONTAL
                     || new2_y < 0 || new2_y >= NUM_CELLS_VERTICAL
-                    || grid[new2_x][new2_y] === CELL_OCCUPIED2
-                ) {
+                    || grid[new2_x][new2_y] || grid[new1_x][new1_y]=== CELL_OCCUPIED2 && CELL_OCCUPIED1 ) {
                     lightCycle2_alive = false
                     endGame = true;
-                    console.log("2"+lightCycle2_alive);
+                    console.log("Joueur 2 à perdu");
         
-                }
-        
-                else {
+                } /**else if () {
+
+                        lightCycle1_alive = false;
+                        lightCycle2_alive = false
+
+                        console.log("match nulle ");
+
+
+                }*/ else {
                     //Position occuper moto 1
                     grid[new1_x][new1_y] = CELL_OCCUPIED1;
                     //Position occuper moto 2
@@ -428,10 +446,14 @@
                     lightCycle2_x = new2_x;
                     lightCycle2_y = new2_y;
                 }
+                gamefinish();
                 redraw();
                 
-            }
-        }      
+            }  
+            
+        
+        }
+          
 
          
       function startInterval(){  
@@ -449,7 +471,7 @@
             refreshIntervalId = setTimeout(function() { run(); }, refreshRate);
         }, 100);
       }
-}
+
 
     
-
+}
